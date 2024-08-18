fc6987e55405e9368a65ec6ff31eaf343b8cdc84
sergisalv <sergisalv@gmail.com>
2024-08-18 17:56:53 +0200

    Creando el método login

--- src/main/java/academy/atl/customers/controller/AuthController.java
+++ src/main/java/academy/atl/customers/controller/AuthController.java
@@ -1,8 +1,25 @@
 package academy.atl.customers.controller;
 
+import academy.atl.customers.dto.RequestLogin;
+import academy.atl.customers.entities.User;
+import academy.atl.customers.services.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class AuthController {
 
-    public void login(String email, String password){
+    @Autowired
+    private AuthService service;
 
+    @PostMapping("/auth/login")
+    public User login(@RequestBody RequestLogin request){
+        String email = request.getEmail();
+        String password = request.getPassword();
+       User user = service.login(email,password);
+       return user;
     }
 }
